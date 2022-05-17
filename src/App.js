64e1809a37a5d15374fdde5efad942093ed91f17
Ryan Gilmour <32744212+gilmoid@users.noreply.github.com>
2022-05-16 20:41:55 -0400

    bentzboy887 cover and original

--- src/App.js
+++ src/App.js
@@ -270,6 +270,8 @@ class Home extends React.Component {
         <SongPlayer songSource={'https://firebasestorage.googleapis.com/v0/b/coverpool-c532b.appspot.com/o/7R5T6CBpJHZF1wITzyLgWhvdfEI3%2Fcovers%2Fdoahnean%2Fdandelion%2Fdandelion%20written%20by%20doahnean%20and%20performed%20by%20mallard%20westcott%20on%20banjo.mp3?alt=media&token=12198219-0485-4018-870c-31a816f66665'} songName = "Dandelion" />
 
         <h3>User Activity: </h3>
+        <p><a href="/user/bentzboy887">bentzboy887</a> uploaded <a href="/user/bentzboy887/originals/T.M.L">T.M.L</a></p>
+        <p><a href="/user/bentzboy887">bentzboy887</a> posted a <a href="/user/bentzboy887/covers/sammy77/kill-rock-stars-2"> cover of kill rock stars 2 </a> by <a href="/user/sammy77">sammy77</a></p>
         <p><a href="/user/sammy77">sammy77</a> uploaded <a href="/user/sammy77/originals/allright">allright</a></p>
         <p><a href="/user/sammy77">sammy77</a> posted a <a href="/user/sammy77/covers/mott/Bald-patch"> cover of Bald patch </a> by <a href="/user/mott">mott</a></p>
         <p><a href="/user/Anry">Anry</a> uploaded <a href="/user/Anry/originals/Sober-Joey">Sober Joey</a></p>
